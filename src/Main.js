23d3148877aa17b14d18c0fa3d2438df6fa38392
Chris <chl9430@gmail.com>
2022-09-21 02:22:36 +0900

    chart 마크업 작업

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,11 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { IoChevronBackSharp, IoChevronForwardSharp } from "react-icons/io5";
 
-import Header from "./components/Header"
-import ExploreCard from "./components/ExploreCard"
+import Header from "./components/Header";
+import ExploreCard from "./components/ExploreCard";
 
 const MainStyle = styled.div`
-.explore_group {
+  .explore_group {
     background: green;
     display: flex;
     position: relative;
@@ -13,44 +13,78 @@ const MainStyle = styled.div`
     margin: 0px 32px;
 
     button {
-        position: absolute;
-        bottom: 50%;
-        z-index: 1;
-        background: white;
-        border: none;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 48px;
-        height: 48px;
-        border-radius: 50%;
-        box-shadow: rgb(0 0 0 / 15%) 0px 4px 10px;
+      position: absolute;
+      bottom: 50%;
+      z-index: 1;
+      background: white;
+      border: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 48px;
+      height: 48px;
+      border-radius: 50%;
+      box-shadow: rgb(0 0 0 / 15%) 0px 4px 10px;
     }
 
     .explore_right {
-        right: -24px;
+      right: -24px;
     }
 
     .explore_left {
-        left: -24px;
+      left: -24px;
     }
-}
-`
+  }
+`;
 
 const Main = () => {
-    return (
+  return (
     <MainStyle>
-        <Header/>
-        <div className="explore_group">
-            <button className="explore_left"><IoChevronBackSharp size="30"/></button>
-            <button className="explore_right"><IoChevronForwardSharp size="30"/></button>
-            <ExploreCard/>
-            <ExploreCard/>
-            <ExploreCard/>
-            <ExploreCard/>
+      <Header />
+      <div className="explore_group">
+        <button className="explore_left">
+          <IoChevronBackSharp size="30" />
+        </button>
+        <button className="explore_right">
+          <IoChevronForwardSharp size="30" />
+        </button>
+        <ExploreCard />
+        <ExploreCard />
+        <ExploreCard />
+        <ExploreCard />
+      </div>
+      <div className="main_chart">
+        <div className="chart_header">
+          <div className="header_type">
+            <button>Trending</button>
+            <button>Top</button>
+          </div>
+          <div className="header_view">
+            <button>24h</button>
+            <button>View all</button>
+          </div>
         </div>
+        <div className="chart_content">
+          <div className="content_list">
+            <div className="list_header">
+              <div>COLLECTION</div>
+              <div>FLOOR PRICE</div>
+              <div>VOLUME</div>
+            </div>
+            <div className="list_item"></div>
+          </div>
+          <div className="content_list">
+            <div className="list_header">
+              <div>COLLECTION</div>
+              <div>FLOOR PRICE</div>
+              <div>VOLUME</div>
+            </div>
+            <div className="list_item"></div>
+          </div>
+        </div>
+      </div>
     </MainStyle>
-    )
-}
+  );
+};
 
 export default Main;
